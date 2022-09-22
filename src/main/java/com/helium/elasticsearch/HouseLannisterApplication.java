15b138ec605c9655541ba9fb9ec505fcfaf7f3b3
hprasad99 <himanshu.prasad@ril.com>
2022-09-22 19:31:30 +0530

    first commit

---
+++ src/main/java/com/helium/elasticsearch/HouseLannisterApplication.java
@@ -0,0 +1,13 @@
+package com.helium.elasticsearch.HouseLannister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseLannisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseLannisterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/helium/elasticsearch/HouseLannister/HouseLannisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helium.elasticsearch.HouseLannister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseLannisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
